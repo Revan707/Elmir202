e5766fab750e180c8b9354c19cf8bb11ffd28904
Ravan G. Huseynov <ravangh@code.local>
2023-02-23 18:02:21 +0400

    task

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 9;
+            int n1 = 0;
+            int n2 = 1;
+            int n3=1;
+           while(n3<num)
+            {
+                Console.WriteLine(n3);
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                
+                
+
+
+                
+            }
+           
+        }
+    }
+}
